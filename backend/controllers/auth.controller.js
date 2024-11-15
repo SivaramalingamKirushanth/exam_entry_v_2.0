bd1386f58589336020964505e1cdd459e6f8c7a0
chamithjeewantha <chamithjeewa123@gmail.com>
2024-11-15 22:25:23 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -87,6 +87,8 @@ export const managerRegister = async (req, res, next) => {
 
   try {
     const password = await generatePassword();
+
+    console.log("Generated password:", password);
     const hashedPassword = await hashPassword(password);
 
     const conn = await pool.getConnection();
