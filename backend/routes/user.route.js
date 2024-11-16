8a8006d07c0059167b1b258d17afe0e8cda1043d
piumal302 <piumal302@gmail.com>
2024-11-16 21:03:44 +0530

    Update user.route.js
    add getAllDeans route and getAllHod route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,6 +4,8 @@ import {
   getAllManagers,
   updateStudent,
   updateManager,
+  getAllHODs,
+  getAllDeans,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
@@ -12,5 +14,8 @@ router.get("/getAllStudents", getAllStudents);
 router.get("/getAllManagers", getAllManagers);
 router.put("/updateStudent", updateStudent);
 router.put("/updateManager", updateManager);
+
+router.get("/getAllHODs", getAllHODs);
+router.get("/getAllDeans", getAllDeans);
 
 export default router;
