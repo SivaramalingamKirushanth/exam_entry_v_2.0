3fc1b5e5c30ee3c53787b8c0509c4e5d1ae60b5e
Chamathka01 <chamathkahettiarachchi@gmail.com>
2024-12-26 15:03:44 +0530

    Defining Routes

--- backend/routes/entry.route.js
+++ backend/routes/entry.route.js
@@ -8,3 +8,17 @@ import {
 import { verifyUser } from "../utils/verifyUsers.js";
 
 const router = express.Router();
+
+router.put("/applyExam", verifyUser(["5"]), applyExam);
+router.post("/getStudentSubjects", verifyUser(["1"]), getStudentSubjects);
+router.post(
+  "/addMedicalResitStudents",
+  verifyUser(["1"]),
+  addMedicalResitStudents
+);
+router.post(
+  "/getStudentsWithoutIndexNumber",
+  verifyUser(["1"]),
+  getStudentsWithoutIndexNumber
+);
+export default router;
