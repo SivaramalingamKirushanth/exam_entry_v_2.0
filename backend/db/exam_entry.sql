094fddcdc4e07e87c73370745ae9907c15f19f05
Liyasdeen Mohammed Zahran <zzzahrannnldeen@gmail.com>
2025-03-06 02:24:02 +0530

    Update exam_entry.sql

--- backend/db/exam_entry.sql
+++ backend/db/exam_entry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 05, 2025 at 09:52 PM
+-- Generation Time: Mar 05, 2025 at 09:53 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
