6250dbfb24650bf53425455869b5e0c8f0b9b7d0
Ilma Fathima <ifilma2001@gmail.com>
2025-04-02 18:09:26 +0530

    Create procedures.sql

---
+++ backend/db/procedures.sql
@@ -0,0 +1,6 @@
+GRANT EXECUTE ON exam_entry.* TO 'student'@'%';
+GRANT EXECUTE ON exam_entry.* TO 'manager'@'%';
+GRANT EXECUTE ON exam_entry.* TO 'hod'@'%';
+GRANT EXECUTE ON exam_entry.* TO 'dean'@'%';
+GRANT EXECUTE ON exam_entry.* TO 'guest'@'%';
+GRANT EXECUTE ON exam_entry.* TO 'admin'@'%';
