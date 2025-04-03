86b665b1c37c08955301eaf47a3ab9386aada857
Ilma Fathima <ifilma2001@gmail.com>
2025-04-03 11:17:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,6 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const FRONTEND_SERVER = process.env.FRONTEND_SERVER;
-const FRONTEND_PORT = process.env.FRONTEND_PORT;
 const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
@@ -28,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: `http://${FRONTEND_SERVER}:${FRONTEND_PORT}`,
+    origin: `http://${FRONTEND_SERVER}`,
     credentials: true,
   })
 );
