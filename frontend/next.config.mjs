0ee15e079f312296e4c5ee0057d12ab86dcf26d6
Liyasdeen Mohammed Zahran <zzzahrannnldeen@gmail.com>
2025-04-05 11:18:58 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const server = process.env.BACKEND_SERVER;
-const port = process.env.BACKEND_PORT;
 
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*", // When frontend calls "/api/..."
-        destination: `http://${server}:${port}/api1/:path*`, // Forward request to backend
+        destination: `${server}/api1/:path*`, // Forward request to backend
       },
     ];
   },
