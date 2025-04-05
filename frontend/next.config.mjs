37bccc346ecadce3f071ff4cd5d04527afa7e368
Liyasdeen Mohammed Zahran <zzzahrannnldeen@gmail.com>
2025-04-05 10:56:34 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*", // When frontend calls "/api/..."
-        destination: `${server}:${port}/api1/:path*`, // Forward request to backend
+        destination: `http://${server}:${port}/api1/:path*`, // Forward request to backend
       },
     ];
   },
