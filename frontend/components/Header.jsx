86d9ee14b5e8ebaf2559d189ab52cb0c9fc720e8
Liyasdeen Mohammed Zahran <zzzahrannnldeen@gmail.com>
2025-08-01 08:50:34 +0530

    UOV logo changed to color

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import UoV_Logo from "./../images/UoV_Logo.png";
+import UoV_Logo_C from "./../images/UoV_Logo_C.png";
 
 const Header = () => {
   return (
     <div className="fixed h-14 sm:h-16 md:h-18 lg:h-20 z-50 w-full bg-white flex items-center px-5">
       <Image
-        src={UoV_Logo}
+        src={UoV_Logo_C}
         alt="UOV logo"
         className="sm:absolute h-[40px] w-[40px] sm:h-[50px] sm:w-[50px] md:h-[60px] md:w-[60px] lg:h-[70px] lg:w-[70px] sm:left-5"
       />
